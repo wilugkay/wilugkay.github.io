1cdc0526b70b86a2939799567a76563d460b01bb
wilson <kaykaydawilug@gmail.com>
2021-05-18 15:32:00 +0100

    wilug picture gallary

---
+++ wilug.js
@@ -0,0 +1,34 @@
+
+const wilugkay =[{"image":"./image/wilug1.JPEG","title":"wilug one","comment":"wilug in coal city rice mill Enugu"},{"image":"./image/wilug2.JPEG","title":"wilug two","comment":"wilug leaving for church service"},{"image":"./image/wilug3.JPEG","title":"wilug three","comment":"wilug at christ embersy church trans-ekulu enugu"},{"image":"./image/wilug4.JPEG","title":"wilug four","comment":"wilug in the village for a friend traditional marriage"},{"image":"./image/wilug5.JPEG","title":"wilug five","comment":"wilug leaving for Nsukka youths summit"},{"image":"./image/wilug6.JPEG","title":"wilug six","comment":"wilug arrived home safely after attending Nsukka youths summit"},{"image":"./image/wilug7.JPEG","title":"wilug seven","comment":"wilug preparing for church service"},{"image":"./image/wilug8.JPEG","title":"wilug eight","comment":"wilug suppervising building project"},{"image":"./image/wilug9.JPEG","title":"wilug nine","comment":"wilug at a friend mother's burial"},{"image":"./image/wilug10.JPEG","title":"wilug ten","comment":"wilug at udenu Governors unity cup tournament"}]
+
+let counter = 0
+
+let buttonPrevious = document.getElementById("btn-previous")
+buttonPrevious.addEventListener("click", function( e ) {
+    console.log( {e} )
+    counter--
+    if ( counter < 0 ){
+        counter = 9
+    }
+    let imageElement = document.getElementById("img-source")
+    let imageTitleElement = document.getElementById("para-title")
+    let imageSummaryElement = document.getElementById("summary") 
+    imageTitleElement.textContent = wilugkay[counter].title
+    imageSummaryElement.textContent = wilugkay[counter].comment
+    imageElement.setAttribute("src", wilugkay[counter].image)
+})
+
+let buttonNext = document.getElementById("btn-next")
+buttonNext.addEventListener("click", function( e ) {
+    if ( counter > 9){
+        counter = 0
+    }
+    let imageElement = document.getElementById("img-source")
+    let imageTitleElement = document.getElementById("para-title")
+    let imageSummaryElement = document.getElementById("summary")
+    imageTitleElement.textContent = wilugkay[counter].title
+    imageSummaryElement.textContent = wilugkay[counter].comment
+    imageElement.setAttribute("src", wilugkay[counter].image)
+    counter++
+    console.log( counter )
+})
