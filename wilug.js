161eb49f38ff7f4a3a62fe161bd84d0b984872f2
wilson <kaykaydawilug@gmail.com>
2021-05-19 10:13:12 +0100

    wilugkay picture gallary

--- wilug.js
+++ wilug.js
@@ -1,6 +1,8 @@
 
 const wilugkay =[{"image":"./image/wilug1.JPEG","title":"wilug one","comment":"wilug in coal city rice mill Enugu"},{"image":"./image/wilug2.JPEG","title":"wilug two","comment":"wilug leaving for church service"},{"image":"./image/wilug3.JPEG","title":"wilug three","comment":"wilug at christ embersy church trans-ekulu enugu"},{"image":"./image/wilug4.JPEG","title":"wilug four","comment":"wilug in the village for a friend traditional marriage"},{"image":"./image/wilug5.JPEG","title":"wilug five","comment":"wilug leaving for Nsukka youths summit"},{"image":"./image/wilug6.JPEG","title":"wilug six","comment":"wilug arrived home safely after attending Nsukka youths summit"},{"image":"./image/wilug7.JPEG","title":"wilug seven","comment":"wilug preparing for church service"},{"image":"./image/wilug8.JPEG","title":"wilug eight","comment":"wilug suppervising building project"},{"image":"./image/wilug9.JPEG","title":"wilug nine","comment":"wilug at a friend mother's burial"},{"image":"./image/wilug10.JPEG","title":"wilug ten","comment":"wilug at udenu Governors unity cup tournament"}]
 
+
+
 let counter = 0
 
 let buttonPrevious = document.getElementById("btn-previous")
